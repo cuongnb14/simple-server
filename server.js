11b7af25f56f944d520d1b0ed64f4b021bd42b6b
Cuong Nguyen <cuongnb14@gmail.com>
2020-09-01 15:32:07 +0700

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+var os = require("os");
+var hostname = os.hostname();
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+process.on('SIGINT', function() {
+    console.log("Caught interrupt signal");
+    process.exit();
+});
+
+
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(hostname);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
